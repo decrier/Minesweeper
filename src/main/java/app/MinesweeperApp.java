7c365c2a8752d2935915b7c30dd97e24260cb69d
Kolja <decrier@ukr.net>
2025-05-06 00:22:05 +0200

    first commit

--- src/main/java/app/MinesweeperApp.java
+++ src/main/java/app/MinesweeperApp.java
@@ -1,4 +1,25 @@
 package app;
 
-public class MinesweeperApp {
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MinesweeperApp extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/game.fxml"));
+        Parent root = loader.load();
+        primaryStage.setTitle("Minesweeper");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
